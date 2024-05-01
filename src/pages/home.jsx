4a05f3d75f83125af6077ce73eb3ed2fa8674e3a
Diya Sundeep <diyasundeep64@gmail.com>
2024-05-01 13:30:45 +0530

    button set

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,8 @@ import Image from "next/image";
 import car from "./../../public/car.png";
 import "tailwindcss/tailwind.css";
 import { Poppins } from "next/font/google";
-
+import { IoSettingsSharp } from "react-icons/io5";
+import Link from "next/link";
 const poppins = Poppins({
   subsets: ["latin"],
   variable: "--font-inter",
@@ -17,19 +18,31 @@ const Home = () => {
         <Image
           src={car}
           alt="bgImage"
-          className="absolute inset-0 w-full h-full object-cover"
+          className="absolute inset-0 w-full h-full  object-cover "
         />
       </div>
 
-      <div className="absolute inset-0 flex justify-center pt-10">
-        <div className="text-white text-center">
-          <div className="text-7xl mb-4 font-bold ">
-            <p className={`${poppins.variable} font-sans`}>Charge-Z</p>
+      <div className="absolute inset-0 ">
+        <div class="mx-2 flex items-end justify-center p-2 lg:p-5 text-white">
+          <div class="flex flex-1 justify-center ">
+            <div className="flex flex-col space-y-4 pt-6">
+              {" "}
+              <p
+                className={`${poppins.variable} font-sans text-center text-7xl`}
+              >
+                Charge-Z{" "}
+              </p>
+              <p
+                className={`${poppins.variable} font-sans text-center text-xl text-slate-600`}
+              >
+                Your one-stop destination for all things related to your ev!
+              </p>
+            </div>
           </div>
-          <div className="text-xl text-slate-600">
-            <p className={`${poppins.variable} font-sans`}>
-              Your one-stop destination for all things related to your ev!
-            </p>
+          <div class="flex justify-end absolute top-10 right-10">
+            <Link href="/" className="p-4 rounded-lg hover:bg-gray-800">
+              <IoSettingsSharp size={30} />
+            </Link>
           </div>
         </div>
       </div>
